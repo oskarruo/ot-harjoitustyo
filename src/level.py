45391fe7e64fdce64044ea4b95b71e3489a5a194
oskarruo <oskar.ruokolainen@gmail.com>
2022-04-19 22:14:14 +0300

    added enemies, minor pylint fixes

--- src/level.py
+++ src/level.py
@@ -1,9 +1,10 @@
 import pygame
-from sprites.PlayerCube import PlayerCube
-from sprites.Floor import Floor
-from sprites.Wall import Wall
-from sprites.Goal import Goal
+from sprites.player_cube import PlayerCube
+from sprites.floor import Floor
+from sprites.wall import Wall
+from sprites.goal import Goal
 from sprites.pickup import Pickup
+from sprites.enemy import Enemy
 
 class Level:
     def __init__(self, level_map, cell_size, pickup_amount):
@@ -13,6 +14,7 @@ class Level:
         self.walls = pygame.sprite.Group()
         self.goaltiles = pygame.sprite.Group()
         self.pickups = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
         self.all_sprites = pygame.sprite.Group()
         self.pickup_amount = pickup_amount
         self.pickups_collected = 0
@@ -23,8 +25,8 @@ class Level:
         height =  len(level_map)
         width = len(level_map[0])
 
-        for y in range(height):
-            for x in range(width):
+        for y in range(height): # pylint: disable=invalid-name
+            for x in range(width): # pylint: disable=invalid-name
                 cell = level_map[y][x]
                 normalized_x = x * self.cell_size
                 normalized_y = y * self.cell_size
@@ -41,31 +43,52 @@ class Level:
                 elif cell == 4:
                     self.floors.add(Floor(normalized_x, normalized_y))
                     self.pickups.add(Pickup(normalized_x + 7.5, normalized_y + 7.5))
+                elif cell == 5:
+                    self.floors.add(Floor(normalized_x, normalized_y))
+                    self.enemies.add(Enemy(normalized_x + 7.5, normalized_y + 7.5))
+                elif cell == 6:
+                    self.floors.add(Floor(normalized_x, normalized_y))
+                    self.enemies.add(Enemy(normalized_x + 7.5, normalized_y + 7.5, 4, 0))
+                elif cell == 7:
+                    self.floors.add(Floor(normalized_x, normalized_y))
+                    self.enemies.add(Enemy(normalized_x + 7.5, normalized_y + 7.5, 0, 4))
 
-        self.all_sprites.add(self.floors, self.walls, self.goaltiles, self.pickups, self.cube)
-    
-    def move_cube(self, dx=0, dy=0):
+        self.all_sprites.add(self.floors, self.walls, self.goaltiles, self.pickups, self.cube, self.enemies)
+
+    def move_cube(self, dx=0, dy=0): # pylint: disable=invalid-name
         if self.pickup_collect():
             self.pickups_collected += 1
         if self.goal_reached() and self.finish_allowed():
             return True
-        elif not self.move_allowed(dx, dy): 
+        if not self.move_allowed(dx, dy):
             self.cube.rect.move_ip(dx, dy)
-    
-    def move_allowed(self, dx=0, dy=0):
+
+    def move_allowed(self, dx=0, dy=0): # pylint: disable=invalid-name
         self.cube.rect.move_ip(dx, dy)
         collision = pygame.sprite.spritecollide(self.cube, self.walls, False)
         self.cube.rect.move_ip(-dx, -dy)
         return collision
-    
+
     def goal_reached(self):
         return pygame.sprite.spritecollide(self.cube, self.goaltiles, False)
-    
+
     def pickup_collect(self):
         return pygame.sprite.spritecollide(self.cube, self.pickups, True)
-    
+
     def finish_allowed(self):
         if self.pickups_collected >= self.pickup_amount:
             return True
-        else:
-            return False
+        return False
+
+    def update_enemies(self):
+        for enemy in self.enemies:
+            self.enemy_change_dir(enemy)
+            enemy.rect.move_ip(enemy.speed_x, enemy.speed_y)
+
+    def enemy_change_dir(self, enemy):
+        if pygame.sprite.spritecollide(enemy, self.walls, False):
+            enemy.speed_x = -enemy.speed_x
+            enemy.speed_y = -enemy.speed_y
+
+    def check_enemy_collisions(self):
+        return pygame.sprite.spritecollide(self.cube, self.enemies, False)
